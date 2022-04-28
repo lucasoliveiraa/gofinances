36c669950146bc8fde4a63dc2dc1e5992032650c
lucasoliveiraa <lucasjackcosta18@gmail.com>
2022-04-27 23:12:33 -0300

    testes com contexto

--- src/components/Forms/Input/styles.ts
+++ src/components/Forms/Input/styles.ts
@@ -1,14 +1,25 @@
-import styled from 'styled-components/native';
-import { TextInput } from 'react-native';
-import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import styled, { css } from "styled-components/native";
+import { TextInput } from "react-native";
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 
-export const Container = styled(TextInput)`
+interface Props {
+  active: boolean;
+}
+
+export const Container = styled(TextInput)<Props>`
   width: 100%;
   padding: 16px 18px;
   font-size: ${RFValue(14)}px;
-  background-color: ${({theme}) => theme.colors.shape};
-  color: ${({theme}) => theme.colors.text_dark};
-  font-family: ${({theme}) => theme.fonts.regular};
+  background-color: ${({ theme }) => theme.colors.shape};
+  color: ${({ theme }) => theme.colors.text_dark};
+  font-family: ${({ theme }) => theme.fonts.regular};
   border-radius: 5px;
   margin-bottom: 8px;
+
+  ${({ active, theme }) =>
+    active &&
+    css`
+      border-width: 3px;
+      border-color: ${theme.colors.attention};
+    `}
 `;
