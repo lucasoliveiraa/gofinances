03e13afd0fdccf8d699687af96ad39870a7628ea
lucasoliveiraa <lucasjackcosta18@gmail.com>
2022-06-23 21:05:40 -0300

    chore: teste com contexto

--- src/components/Forms/Input/input.spec.tsx
+++ src/components/Forms/Input/input.spec.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { render } from "@testing-library/react-native";
 import { Input } from ".";
+import { ThemeProvider } from "styled-components/native";
+import theme from "../../../global/styles/theme";
+
+const Providers: React.FC = ({ children }) => (
+  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+);
 
 describe("Input Component", () => {
   it("must have specific border color when active", () => {
@@ -11,7 +17,10 @@ describe("Input Component", () => {
         keyboardType="email-address"
         autoCorrect={false}
         active={true}
-      />
+      />,
+      {
+        wrapper: Providers,
+      }
     );
 
     const inputComponent = getByTestId("input-email");
